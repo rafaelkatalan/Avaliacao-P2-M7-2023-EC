9844210535980ab59b30577dd56ed7905a07b516
Murilo-ZC <carvalho.zanini@gmail.com>
2023-09-19 22:38:49 -0300

    base da aplicacao

--- database/criar_tabelas.sql
+++ database/criar_tabelas.sql
@@ -1,9 +1,8 @@
-IF EXISTS minhas_notas
-    DROP TABLE minhas_notas;
+DROP TABLE IF EXISTS minhas_notas ;
 
 CREATE TABLE minhas_notas (
     id SERIAL PRIMARY KEY,
     titulo VARCHAR(255) NOT NULL,
-    descricao TEXT NOT NULL
+    descricao TEXT NOT null,
     data_criacao TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
--- frontend/script.js
+++ frontend/script.js
@@ -1,29 +1,62 @@
 function addTask() {
-    const taskInput = document.getElementById("taskInput");
-    const taskText = taskInput.value.trim();
+  const taskInput = document.getElementById("taskInput");
+  const taskText = taskInput.value.trim();
 
-    if (taskText !== "") {
-        const taskList = document.getElementById("taskList");
+  if (taskText !== "") {
+    const taskList = document.getElementById("taskList");
+    const newTaskItem = document.createElement("li");
+
+    // Criar elemento de texto da tarefa
+    const taskTextElement = document.createElement("span");
+    taskTextElement.textContent = taskText;
+    newTaskItem.appendChild(taskTextElement);
+
+    // Botão para excluir a tarefa
+    const deleteButton = document.createElement("button");
+    deleteButton.textContent = "Excluir";
+    deleteButton.addEventListener("click", function () {
+      taskList.removeChild(newTaskItem);
+    });
+
+    newTaskItem.appendChild(deleteButton);
+
+    // Adicionar a nova tarefa à lista
+    taskList.appendChild(newTaskItem);
+
+    // Limpar o campo de entrada
+    taskInput.value = "";
+  }
+}
+
+function getTasks() {
+  const taskList = document.getElementById("taskList");
+  //Limpa a lista
+  taskList.innerHTML = "";
+  fetch("http://localhost:8000/notes")
+    .then((response) => response.json())
+    .then((data) => {
+      data["data"].forEach((task) => {
         const newTaskItem = document.createElement("li");
 
         // Criar elemento de texto da tarefa
         const taskTextElement = document.createElement("span");
-        taskTextElement.textContent = taskText;
+        taskTextElement.textContent = `${task[1]} : ${task[2]}  `;
         newTaskItem.appendChild(taskTextElement);
 
         // Botão para excluir a tarefa
         const deleteButton = document.createElement("button");
         deleteButton.textContent = "Excluir";
         deleteButton.addEventListener("click", function () {
-            taskList.removeChild(newTaskItem);
+          taskList.removeChild(newTaskItem);
         });
-        
+
         newTaskItem.appendChild(deleteButton);
 
         // Adicionar a nova tarefa à lista
         taskList.appendChild(newTaskItem);
-
-        // Limpar o campo de entrada
-        taskInput.value = "";
-    }
+      });
+    })
+    .catch((error) => {
+      console.error("Erro:", error);
+    });
 }
